9f8f47e91a21fb12df8f027717e507e4cd71d2e7
Martin Tracey <matracey@outlook.ie>
2025-03-10 19:46:19 -0700

    feat(common): add EnumerableHelpers with ForEach extension method for IEnumerable collections

---
+++ src/RwzReader.Common/Helpers/EnumerableHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace RwzReader.Common.Helpers;
+
+/// <summary>
+/// Provides extension methods for <see cref="IEnumerable{T}"/> collections.
+/// </summary>
+public static class EnumerableHelpers
+{
+    /// <summary>
+    /// Performs the specified action on each element of the <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the enumerable.</typeparam>
+    /// <param name="source">The enumerable source to iterate over.</param>
+    /// <param name="action">The action to perform on each element.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="source"/> or <paramref name="action"/> is null.
+    /// </exception>
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(action);
+
+        foreach (var item in source)
+        {
+            action(item);
+        }
+    }
+}
